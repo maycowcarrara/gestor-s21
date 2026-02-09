10c9c4203a93ee69667223507e75fef145bb909f
maycowcarrara <48160886+maycowcarrara@users.noreply.github.com>
2026-02-09 16:42:26 -0300

    env

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,14 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// COLE AQUI O QUE VOCÊ COPIOU DO CONSOLE
+// Configuração lendo as variáveis de ambiente (Vite)
 const firebaseConfig = {
-    apiKey: "AIzaSyDzr5QoKTQEPcYYrnpLEabubUpDKSndML4",
-    authDomain: "gestor-s21.firebaseapp.com",
-    projectId: "gestor-s21",
-    storageBucket: "gestor-s21.firebasestorage.app",
-    messagingSenderId: "36719474744",
-    appId: "1:36719474744:web:d0ef94650768f044054ecd"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID
 };
 
 // Inicializa o App
